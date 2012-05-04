6a502b854a099a22073f8be8984cf5a3ee28189c
chrisB <chrismonstah@googlemail.com>
2012-05-04 11:38:51 +0200

    tomcat, blazeds, puremcv

---
+++ tomcat/webapps/blazeds/WEB-INF/src/mme2/nursead/model/PatientVO.java
@@ -0,0 +1,14 @@
+package mme2.nursead.model;
+
+public class PatientVO {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
